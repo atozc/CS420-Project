826bec8b2bbb6fc9937938074bdeb045fc201c50
atozc <catoz@nevada.unr.edu>
2022-04-16 15:36:29 -0700

    Added some panel scripts, and worked on panel prefabs

---
+++ Assets/Scripts/TestPanel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = GetComponent<GamePanel>();
+    }
+
+    public GamePanel panel;
+    public bool test;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 100 == 0)
+            panel.isVisible = test;
+    }
+}
