a3caa73af42a9a8951d42d194e87b3d6a030622a
yongyi-z <64576317+yongyi-z@users.noreply.github.com>
2022-05-06 02:31:21 -0700

    planets
    Okay so OpenPanel is for clicking on the planets to bring up the planet's panel which I don't think works but I also don't really know how to test it out.
    PlanetTrigger is closer to what we wanted cause it should bring up the planet's panel if the rocket "collides" with the planet but the rocket doesn't fly so I also couldnt test this.
    They all use SetActive cause I based it on like the pause menu tutorials I saw which was all I could find on switching panels so as it is now the planet panels would have to move ig
    idk sorry I tried

---
+++ Assets/Scripts/OpenPanel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void PanelOpen()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
