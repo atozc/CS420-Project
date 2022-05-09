89b6688f134ee59da084923a761a97fc7f183594
atozc <catoz@nevada.unr.edu>
2022-05-09 15:45:07 -0700

    Fixed planet triggers script/pausing, release build 2.0

--- Assets/Scripts/NewPlanetTrigger.cs
+++ Assets/Scripts/NewPlanetTrigger.cs
@@ -4,21 +4,165 @@ using UnityEngine;
 
 public class NewPlanetTrigger : MonoBehaviour
 {
-    public GameObject PlanetUI;
+    public GameObject MercuryUI;
+    public GameObject VenusUI;
+    public GameObject EarthUI;
+    public GameObject MarsUI;
+    public GameObject JupiterUI;
+    public GameObject SaturnUI;
+    public GameObject UranusUI;
+    public GameObject NeptuneUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButton(0))
         {
-            PlanetUI.SetActive(true);
-            Time.timeScale = 0;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Casts the ray and get the first game object hit
+            Physics.Raycast(ray, out hit);
+            //Debug.Log("This hit at " + hit.point);
+            Debug.Log("This hit planet " + gameObject.name);
+
+            if (gameObject.name == "Mercury")
+            {
+                MercuryUI.SetActive(true); //set associated planet's UI active
+
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Venus")
+            {
+                VenusUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Earth")
+            {
+                EarthUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Mars")
+            {
+                MarsUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Jupiter")
+            {
+                JupiterUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Saturn")
+            {
+                SaturnUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                UranusUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Uranus")
+            {
+                UranusUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                NeptuneUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
+            if (gameObject.name == "Neptune")
+            {
+                NeptuneUI.SetActive(true); //set associated planet's UI active
+
+                MercuryUI.SetActive(false);
+                VenusUI.SetActive(false);
+                EarthUI.SetActive(false);
+                MarsUI.SetActive(false);
+                JupiterUI.SetActive(false);
+                SaturnUI.SetActive(false);
+                UranusUI.SetActive(false);
+
+                Time.timeScale = 0; //pause gameplay
+            }
         }
     }
+
+    public void GoBackToGame()
+    {
+        MercuryUI.SetActive(false);
+        VenusUI.SetActive(false);
+        EarthUI.SetActive(false);
+        MarsUI.SetActive(false);
+        JupiterUI.SetActive(false);
+        SaturnUI.SetActive(false);
+        UranusUI.SetActive(false);
+        NeptuneUI.SetActive(false);
+        Time.timeScale = 1;
+    }
 }
+          
--- Assets/Scripts/PauseMenuMgr.cs
+++ Assets/Scripts/PauseMenuMgr.cs
@@ -8,22 +8,38 @@ public class PauseMenuMgr : MonoBehaviour
     public GameObject pauseMenuUI;
     public GameObject audioMenuUI;
 
+    public static bool isGamePaused = false;
+
     // Update is called once per frame
     void Update()
     {
-      
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        isGamePaused = false;
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+        isGamePaused = true;
     }
 
     public void AudioSettings()
